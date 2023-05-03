2b7b781dbe5bdf163ebdfd4e5a7f03727ddee798
code7a <115498258+code7a@users.noreply.github.com>
2023-05-03 16:22:10 -0400

    commit

--- create-pce-certificate.sh
+++ create-pce-certificate.sh
@@ -1,16 +1,20 @@
 #create-certificate.sh
+cat << EOF >> /etc/rc.local
 #sleep for creation of dns record on name servers
 sleep 10
+#create certificate
 #if dev, sign with test CAs
-if [[ $(hostname) != *".dev."* ]]; then
-    certbot certonly --domain $(hostname) --manual --preferred-challenges dns --manual-auth-hook /.certbot_authenticator.sh --manual-cleanup-hook /.certbot_cleanup.sh --agree-tos --register-unsafely-without-email --force-renewal --key-type rsa
+if [[ \$(hostname) != *".dev."* ]]; then
+    certbot certonly --domain \$(hostname) --manual --preferred-challenges dns --manual-auth-hook /.certbot_authenticator.sh --manual-cleanup-hook /.certbot_cleanup.sh --agree-tos --register-unsafely-without-email --force-renewal --key-type rsa
 else
-    certbot certonly --domain $(hostname) --manual --preferred-challenges dns --manual-auth-hook /.certbot_authenticator.sh --manual-cleanup-hook /.certbot_cleanup.sh --agree-tos --register-unsafely-without-email --force-renewal --key-type rsa --test-cert
+    certbot certonly --domain \$(hostname) --manual --preferred-challenges dns --manual-auth-hook /.certbot_authenticator.sh --manual-cleanup-hook /.certbot_cleanup.sh --agree-tos --register-unsafely-without-email --force-renewal --key-type rsa --test-cert
 fi
-certbot_result=$(echo $?)
+certbot_result=\$(echo \$?)
 #if error, sleep 60, retry
-echo $certbot_result|grep 0 || sleep 60 && certbot certonly --domain $(hostname) --manual --preferred-challenges dns --manual-auth-hook /.certbot_authenticator.sh --manual-cleanup-hook /.certbot_cleanup.sh --agree-tos --register-unsafely-without-email --force-renewal --key-type rsa --test-cert
-echo y|cp /etc/letsencrypt/live/$(hostname)/cert.pem /var/lib/illumio-pce/cert/server.crt
-echo y|cp /etc/letsencrypt/live/$(hostname)/privkey.pem /var/lib/illumio-pce/cert/server.key
+echo \$certbot_result | grep 0 || sleep 60 && certbot certonly --domain \$(hostname) --manual --preferred-challenges dns --manual-auth-hook /.certbot_authenticator.sh --manual-cleanup-hook /.certbot_cleanup.sh --agree-tos --register-unsafely-without-email --force-renewal --key-type rsa --test-cert
+#copy, set permissions
+echo y|cp /etc/letsencrypt/live/\$(hostname)/cert.pem /var/lib/illumio-pce/cert/server.crt
+echo y|cp /etc/letsencrypt/live/\$(hostname)/privkey.pem /var/lib/illumio-pce/cert/server.key
 chmod 400 /var/lib/illumio-pce/cert/server.key
 chown ilo-pce:ilo-pce /var/lib/illumio-pce/cert/server.key
+EOF
