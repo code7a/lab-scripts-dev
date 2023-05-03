acca256d1c44171162c343e4afb6d582c7ebcc46
code7a <115498258+code7a@users.noreply.github.com>
2023-05-03 17:06:24 -0400

    commit

--- aws-cli-install.sh
+++ aws-cli-install.sh
@@ -4,4 +4,6 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 ./aws/install
 export PATH=/usr/local/bin/:$PATH
-aws configure
+mkdir .aws
+echo "aws_access_key_id = $aws_key
+aws_secret_access_key = $aws_secret" > .aws/config
