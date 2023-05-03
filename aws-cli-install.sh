182147b1caf6707f66045dda3bed1ae3b49d2e5f
code7a <115498258+code7a@users.noreply.github.com>
2023-05-03 17:14:56 -0400

    commit

--- aws-cli-install.sh
+++ aws-cli-install.sh
@@ -1,9 +1,14 @@
 #aws-cli-install.sh
 yum install -y zip jq
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+unzip -u awscliv2.zip
 ./aws/install
 export PATH=/usr/local/bin/:$PATH
 mkdir .aws
-echo "aws_access_key_id = $aws_key
-aws_secret_access_key = $aws_secret" > .aws/config
+cat << EOF > .aws/config
+[default]
+EOF
+cat << EOF > .aws/credentials
+aws_access_key_id = $aws_key
+aws_secret_access_key = $aws_secret
+EOF
