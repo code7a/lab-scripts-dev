91723994b3ff0147950928a09747d8bbfd4c44ff
code7a <115498258+code7a@users.noreply.github.com>
2023-05-03 16:18:50 -0400

    commit

---
+++ create-pce-certificate.sh
@@ -0,0 +1,16 @@
+#create-certificate.sh
+#sleep for creation of dns record on name servers
+sleep 10
+#if dev, sign with test CAs
+if [[ $(hostname) != *".dev."* ]]; then
+    certbot certonly --domain $(hostname) --manual --preferred-challenges dns --manual-auth-hook /.certbot_authenticator.sh --manual-cleanup-hook /.certbot_cleanup.sh --agree-tos --register-unsafely-without-email --force-renewal --key-type rsa
+else
+    certbot certonly --domain $(hostname) --manual --preferred-challenges dns --manual-auth-hook /.certbot_authenticator.sh --manual-cleanup-hook /.certbot_cleanup.sh --agree-tos --register-unsafely-without-email --force-renewal --key-type rsa --test-cert
+fi
+certbot_result=$(echo $?)
+#if error, sleep 60, retry
+echo $certbot_result|grep 0 || sleep 60 && certbot certonly --domain $(hostname) --manual --preferred-challenges dns --manual-auth-hook /.certbot_authenticator.sh --manual-cleanup-hook /.certbot_cleanup.sh --agree-tos --register-unsafely-without-email --force-renewal --key-type rsa --test-cert
+echo y|cp /etc/letsencrypt/live/$(hostname)/cert.pem /var/lib/illumio-pce/cert/server.crt
+echo y|cp /etc/letsencrypt/live/$(hostname)/privkey.pem /var/lib/illumio-pce/cert/server.key
+chmod 400 /var/lib/illumio-pce/cert/server.key
+chown ilo-pce:ilo-pce /var/lib/illumio-pce/cert/server.key
