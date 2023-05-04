f79e561f4f367fb9df65d8048b22ebad185f2935
code7a <115498258+code7a@users.noreply.github.com>
2023-05-04 16:56:39 -0400

    commit

--- install-pce.sh
+++ install-pce.sh
@@ -1,6 +1,6 @@
 #install-pce.sh
 #modify to get bin repo script
-yum install -y wget bzip2 net-tools initscripts libxcrypt-compat compat-openssl11
+yum install -y wget bzip2 net-tools initscripts libxcrypt-compat
 
 cd /
 wget https://$repo/23.2/GA%20Releases/23.2.0/pce/pkgs/illumio-pce-23.2.0-1346.c8.x86_64.rpm
@@ -18,7 +18,7 @@ echo y|cp /etc/letsencrypt/live/$(hostname)/privkey.pem /var/lib/illumio-pce/cer
 chmod 400 /var/lib/illumio-pce/cert/server.key
 chown ilo-pce:ilo-pce /var/lib/illumio-pce/cert/server.key
 
-/opt/illumio-pce/illumio-pce-env setup --batch node_type='snc0' email_address=$pce_admin_username_email_address pce_fqdn=$vm_hostname metrics_collection_enabled=false expose_user_invitation_link=true
+/opt/illumio-pce/illumio-pce-env setup --batch node_type='snc0' email_address=$pce_admin_username_email_address pce_fqdn=$(hostname) metrics_collection_enabled=false expose_user_invitation_link=true
 sudo -u ilo-pce /opt/illumio-pce/illumio-pce-ctl start --runlevel 1 &> /dev/null
 sleep 120
 sudo -u ilo-pce /opt/illumio-pce/illumio-pce-ctl status -w
@@ -32,7 +32,7 @@ sudo -u ilo-pce /opt/illumio-pce/illumio-pce-ctl status -w
 sleep 10
 sudo -u ilo-pce /opt/illumio-pce/illumio-pce-ctl status -w
 #create domain
-sudo -u ilo-pce ILO_PASSWORD=$pce_admin_password /opt/illumio-pce/illumio-pce-db-management create-domain --user-name $pce_admin_username_email_address --full-name admin --org-name $vm_hostname
+sudo -u ilo-pce ILO_PASSWORD=$pce_admin_password /opt/illumio-pce/illumio-pce-db-management create-domain --user-name $pce_admin_username_email_address --full-name admin --org-name $(hostname)
 #install ven bundle
 sudo -u ilo-pce /opt/illumio-pce/illumio-pce-ctl ven-software-install /illumio-ven-bundle-* --compatibility-matrix /illumio-release-compatibility-* --default --no-prompt --orgs 1
 sleep 10 && systemctl restart sshd &
