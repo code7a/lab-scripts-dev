3ede1be1303848bb4e5048af7110cf09bf616a1c
code7a <115498258+code7a@users.noreply.github.com>
2023-05-03 20:20:32 -0400

    commit

--- create-certificate.sh
+++ create-certificate.sh
@@ -52,9 +52,5 @@ fi
 certbot_result=\$(echo \$?)
 #if error, sleep 60, retry
 echo \$certbot_result | grep 0 || sleep 60 && certbot certonly --domain \$(hostname) --manual --preferred-challenges dns --manual-auth-hook /.certbot_authenticator.sh --manual-cleanup-hook /.certbot_cleanup.sh --agree-tos --register-unsafely-without-email --force-renewal --key-type rsa --test-cert
-#copy, set permissions
-echo y|cp /etc/letsencrypt/live/\$(hostname)/cert.pem /var/lib/illumio-pce/cert/server.crt
-echo y|cp /etc/letsencrypt/live/\$(hostname)/privkey.pem /var/lib/illumio-pce/cert/server.key
-chmod 400 /var/lib/illumio-pce/cert/server.key
-chown ilo-pce:ilo-pce /var/lib/illumio-pce/cert/server.key
 EOF
+chmod +x /etc/rc.local
