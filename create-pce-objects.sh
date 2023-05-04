e7a96776572cb01d3cfa8aa6a1d6873b2950b439
code7a <115498258+code7a@users.noreply.github.com>
2023-05-04 16:47:49 -0400

    commit

---
+++ create-pce-objects.sh
@@ -0,0 +1,19 @@
+#create-pce-objects.sh
+yum install -y jq
+basic_auth_token=$(echo -n "$pce_admin_username_email_address:$pce_admin_username_email_address"|base64)
+auth_token=$(curl -X POST -H "Authorization: Basic $basic_auth_token" https://$(hostname):8443/api/v2/login_users/authenticate?pce_fqdn=$(hostname) | jq -r '.auth_token')
+login_response=$(curl -H "Authorization: Token token=$auth_token" https://$(hostname):8443/api/v2/users/login)
+auth_username=$(echo $login_response | jq -r '.auth_username')
+session_token=$(echo $login_response | jq -r '.session_token')
+#update default pairing profile, selective, allow labels, allow enforcement
+curl -u $auth_username:$session_token https://$(hostname):8443/api/v2/orgs/1/pairing_profiles/1 -X PUT -H 'Content-Type: application/json' --data-raw '{"name":"default","description":"Default Pairing Profile","labels":[],"enforcement_mode":"selective","visibility_level":"flow_summary","allowed_uses_per_key":"unlimited","agent_software_release":null,"key_lifespan":"unlimited","app_label_lock":false,"env_label_lock":false,"loc_label_lock":false,"role_label_lock":false,"enforcement_mode_lock":false,"visibility_level_lock":true,"enabled":true}'
+#create nginx app label
+curl -u $auth_username:$session_token https://$(hostname):8443/api/v2/orgs/1/labels -H 'Content-Type: application/json' --data-raw '{"key":"app","value":"A-NGINX"}'
+#create share app label
+curl -u $auth_username:$session_token https://$(hostname):8443/api/v2/orgs/1/labels -H 'Content-Type: application/json' --data-raw '{"key":"app","value":"A-SHARE"}'
+#get service
+service_href=$(curl -u $auth_username:$session_token "https://$(hostname):8443/api/v2/orgs/1/sec_policy/active/services?name=S-NETBIOS" | jq -r .[].href)
+#create enforcement boundry
+curl -u $auth_username:$session_token https://$(hostname):8443/api/v2/orgs/1/sec_policy/draft/enforcement_boundaries -X POST -H 'Content-Type: application/json' --data-raw '{"name":"block-netbios","enabled":true,"providers":[{"actors":"ams"}],"consumers":[{"ip_list":{"href":"/orgs/1/sec_policy/draft/ip_lists/1"}}],"network_type":"brn","ingress_services":[{"href":"'$service_href'"}]}'
+#provision
+curl -u $auth_username:$session_token https://$(hostname):8443/api/v2/orgs/1/sec_policy -X POST -H 'Content-Type: application/json' --data-raw '{"update_description":"","change_subset":{"enforcement_boundaries":[{"href":"/orgs/1/sec_policy/draft/enforcement_boundaries/1"}]}}'
--- install-pce.sh
+++ install-pce.sh
@@ -1,6 +1,6 @@
 #install-pce.sh
 #modify to get bin repo script
-yum install -y wget bzip2 net-tools initscripts
+yum install -y wget bzip2 net-tools initscripts libxcrypt-compat compat-openssl11
 
 cd /
 wget https://$repo/23.2/GA%20Releases/23.2.0/pce/pkgs/illumio-pce-23.2.0-1346.c8.x86_64.rpm
@@ -10,6 +10,8 @@ wget https://$repo/23.2/GA%20Releases/23.2.0/ven/bundle/illumio-ven-bundle-23.2.
 cd
 rpm -Uvh /illumio-pce-*.rpm
 #mkdir /opt/illumio-pce/ && tar -xf illumio-pce-*.tgz -C /opt/illumio-pce/ && chown -R root:ilo-pce /opt/illumio-pce/
+
+#file kernal settings
 
 echo y|cp /etc/letsencrypt/live/$(hostname)/cert.pem /var/lib/illumio-pce/cert/server.crt
 echo y|cp /etc/letsencrypt/live/$(hostname)/privkey.pem /var/lib/illumio-pce/cert/server.key
@@ -24,7 +26,6 @@ sleep 10
 sudo -u ilo-pce /opt/illumio-pce/illumio-pce-ctl status -w
 sudo -u ilo-pce /opt/illumio-pce/illumio-pce-db-management setup
 sleep 10
-2>/dev/null >/dev/null </dev/null
 sudo -u ilo-pce /opt/illumio-pce/illumio-pce-ctl set-runlevel 5 &> /dev/null
 sleep 60
 sudo -u ilo-pce /opt/illumio-pce/illumio-pce-ctl status -w
