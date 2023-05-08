3f9f4fb8a5548e4ea46f8d914821a0c211a95217
code7a <115498258+code7a@users.noreply.github.com>
2023-05-08 15:51:39 -0400

    commit

--- aws-cli-install.sh
+++ aws-cli-install.sh
@@ -1,7 +1,7 @@
 #aws-cli-install.sh
 yum install -y zip jq
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip -u awscliv2.zip
+unzip -uq awscliv2.zip
 ./aws/install --update
 export PATH=/usr/local/bin/:$PATH
 mkdir .aws >/dev/null 2>&1 || true
