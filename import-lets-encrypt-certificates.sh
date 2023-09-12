853feaa2d4d52a0087be8e510cf70af611940fa8
code7a <115498258+code7a@users.noreply.github.com>
2023-09-11 20:36:02 -0400

    k3s scripts

--- import-lets-encrypt-certificates.sh
+++ import-lets-encrypt-certificates.sh
@@ -2,7 +2,7 @@
 curl https://letsencrypt.org/certs/isrgrootx1.pem -o /etc/pki/ca-trust/source/anchors/isrgrootx1.pem
 curl https://letsencrypt.org/certs/lets-encrypt-r3.pem -o /etc/pki/ca-trust/source/anchors/lets-encrypt-r3.pem
 curl https://letsencrypt.org/certs/staging/letsencrypt-stg-int-r3.pem -o /etc/pki/ca-trust/source/anchors/letsencrypt-stg-int-r3.pem
-curl https://letsencrypt.org/certs/staging/letsencrypt-stg-int-e1.pem-o /etc/pki/ca-trust/source/anchors/letsencrypt-stg-int-e1.pem
+curl https://letsencrypt.org/certs/staging/letsencrypt-stg-int-e1.pem -o /etc/pki/ca-trust/source/anchors/letsencrypt-stg-int-e1.pem
 curl https://letsencrypt.org/certs/staging/letsencrypt-stg-root-x1.pem -o /etc/pki/ca-trust/source/anchors/letsencrypt-stg-root-x1.pem
 curl https://letsencrypt.org/certs/staging/letsencrypt-stg-root-x2.pem -o /etc/pki/ca-trust/source/anchors/letsencrypt-stg-root-x2.pem
 update-ca-trust enable && update-ca-trust extract
