9b0a4da83139602452bf7ad7b442b394581cdcfa
code7a <115498258+code7a@users.noreply.github.com>
2024-03-04 12:34:03 -0500

    k3s

--- userdata/install-k3.sh
+++ userdata/install-k3.sh
@@ -7,3 +7,5 @@ source .bash_profile
 echo 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' > /etc/profile.d/kubeconfig.sh
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 service k3s restart
+#shutdown in 8 hours
+shutdown +480
