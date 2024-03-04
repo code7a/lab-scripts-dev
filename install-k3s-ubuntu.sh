96d121a8a09375da1db46a107b28582f7555560e
code7a <115498258+code7a@users.noreply.github.com>
2024-03-04 11:33:16 -0500

    k3s

---
+++ install-k3s-ubuntu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#install k3s - ubuntu
+apt update -y
+curl -sfL https://get.k3s.io | sh -
+echo 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' > .bash_profile
+source .bash_profile
+echo 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' > /etc/profile.d/kubeconfig.sh
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+service k3s restart
